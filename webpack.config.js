a5684171724ea51d5710314f9189939e8652f416
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-09-06 16:27:38 +0100

    Support ES6

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-/* eslint-env node */
-
 module.exports = {
     entry: {
         'google-calendar': './scripts/google-calendar',
         autocomplete: './scripts/autocomplete.js'
     },
-    output: {
-        path: 'scripts/dist',
-        filename: '[name].js'
-    }
+    output: {path: 'scripts/dist', filename: '[name].js'},
+    module: {loaders: [{
+        include: __dirname + '/scripts',
+        test: /\.js$/,
+        loader: 'babel-loader'
+    }]},
+    devtool: 'source-map'
 };
