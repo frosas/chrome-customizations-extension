95b95db31c6a866ccecfe3def1fb23063ae3b9f7
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-09-06 21:58:05 +0100

    Hide candidates if there are no candidates

--- scripts/autocomplete/CandidatesElement.js
+++ scripts/autocomplete/CandidatesElement.js
@@ -1,7 +1,6 @@
 module.exports = class CandidatesElement {
     constructor() {
         this._tooltip = document.createElement('div');
-        this._tooltip.style.display = 'none';
         this._tooltip.style.position = 'fixed';
         this._tooltip.style.left = '10px';
         this._tooltip.style.top = '10px';
@@ -9,13 +8,26 @@ module.exports = class CandidatesElement {
         this._tooltip.style.backgroundColor = '#fffcb6';
         this._tooltip.style.opacity = 0.9;
         this._tooltip.style.zIndex = 9999;
+        this._hide();
         document.body.appendChild(this._tooltip);
     }
 
     set candidates(candidates) {
-        this._tooltip.innerText = candidates.join('\n');
+        if (candidates.length) {
+            this._tooltip.innerText = candidates.join('\n');
+            this._show();
+            clearTimeout(this._timeoutId);
+            this._timeoutId = setTimeout(() => this._hide(), 1500);
+        } else {
+            this._hide();
+        }
+    }
+
+    _show() {
         this._tooltip.style.display = 'block';
-        clearTimeout(this._timeoutId);
-        this._timeoutId = setTimeout(() => this._tooltip.style.display = 'none', 1500);
+    }
+
+    _hide() {
+        this._tooltip.style.display = 'none';
     }
 }
