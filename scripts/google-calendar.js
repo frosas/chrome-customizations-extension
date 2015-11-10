d7c6dfe44adb3bd5c3e77b933c61f2f7b4d6103b
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-11-10 12:29:50 +0000

    Google Calendar style

--- scripts/google-calendar.js
+++ scripts/google-calendar.js
@@ -1,7 +1,3 @@
-var marked = require('marked');
-
-var lastTaskBubbleSeen;
-
 var toArray = function (collection) {
     return [].map.call(collection, function (item) { return item; });
 };
@@ -20,48 +16,9 @@ var onBubble = function (callback) {
     observer.observe(document.body, {childList: true, subtree: true});
 };
 
-/**
- * @returns [HTMLElement|null]
- */
-var getBubbleTextarea = function (element) {
-    return element.contentDocument.querySelector('textarea.ec');
-};
-
 onBubble(function (element) {
-    var isEditing = function () {
-        return textarea.style.display == 'block';
-    };
-    
-    var toggleEdition = function () {
-        if (isEditing()) {
-            textarea.style.display = 'none';
-        } else {
-            textarea.style.display = 'block';
-            textarea.focus();
-        }
-    };
-    
-    var render = function () {
-        renderedElement.innerHTML = marked(textarea.value);
-    };
-    
     var link = element.contentDocument.createElement('link');
     link.rel = 'stylesheet';
     link.href = chrome.extension.getURL('styles/google-calendar-bubble.css');
     element.contentDocument.head.appendChild(link);
-
-    var textarea = getBubbleTextarea(element);
-    textarea.addEventListener('change', render);
-    textarea.addEventListener('keyup', render);
-    
-    var renderedElement = document.createElement('div');
-    renderedElement.addEventListener('click', function (event) {
-        // Ignore events on element with a default action
-        if (event.target.nodeName == 'A') return; 
-        
-        toggleEdition();
-    });
-    textarea.parentNode.appendChild(renderedElement);
-    
-    render();
 });
