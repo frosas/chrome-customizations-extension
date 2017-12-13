13e31bcc3074671e79ba3a431f9551dca2fd8fff
Francesc Rosas <francescrosasbosque@gmail.com>
2017-12-13 16:37:40 +0100

    Build production version of React

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const { promisify } = require("util");
 const glob = require("glob");
 const { basename, dirname } = require("path");
+const { DefinePlugin } = require("webpack");
 
 module.exports = async () => ({
   entry: (await promisify(glob)("./js/**/entry.js")).reduce((entries, file) => {
     const name = basename(dirname(file)); // Use the parent dir name
     return ({...entries, [name]: file});
   }, {}),
-  output: { filename: "dist/scripts/[name].js" }
+  output: { filename: "dist/scripts/[name].js" },
+  plugins: [
+    // For React
+    new DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify("production") })
+  ]
 });
