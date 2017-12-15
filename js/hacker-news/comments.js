800986257096e66371757d9b94df40c6fb2e4274
Francesc Rosas <francescrosasbosque@gmail.com>
2017-12-15 15:36:33 +0100

    Extract js/hacker-news/comments/TreeNode

--- js/hacker-news/comments.js
+++ js/hacker-news/comments.js
@@ -1,30 +1,4 @@
-const INDENT_WIDTH = 12; // TODO Detect it automatically?
-
-class CommentTreeNode {
-  constructor(attrs) {
-    this.children = [];
-    Object.assign(this, attrs);
-  }
-
-  nest(node) {
-    this.children.push(node);
-    node.parent = this;
-  }
-
-  visit(callback) {
-    callback(this);
-    this.children.forEach(child => child.visit(callback));      
-  }
-
-  // All nodes are comments but the root
-  get isComment() {
-    return this.domNode;
-  }
-
-  get domNodeIndent() {
-    return this.domNode && this.domNode.querySelector('.ind img').width / INDENT_WIDTH;
-  }
-}
+import CommentTreeNode from "./comments/TreeNode";
 
 const getCommentsTree = () => {
   const tree = new CommentTreeNode;
