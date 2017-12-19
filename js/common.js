1bfca9c3fb97fb19477ce1fac1be26a7eb1e8f05
Francesc Rosas <francescrosasbosque@gmail.com>
2017-12-19 12:18:28 +0100

    Expand task descriptions automatically

--- js/common.js
+++ js/common.js
@@ -11,3 +11,17 @@ export const waitForElement = selector => {
   return waitFor(() => document.querySelector(selector));
 };
 
+// TODO Support changes on attributes (see observe() attributes param).
+// TODO Reuse the MutationObserver instance
+export const liveQuerySelectorAll = (baseElement, selector, callback) => {
+  baseElement.querySelectorAll(selector).forEach(callback);
+  new MutationObserver(records => {
+    records.forEach(record => {
+      record.addedNodes.forEach(node => {
+        node = node.parentNode; // Consider the element itself
+        if (!(node instanceof Element)) return;
+        node.querySelectorAll(selector).forEach(callback);
+      });
+    });
+  }).observe(baseElement, { childList: true, subtree: true });
+};
---
+++ js/trello/entry.js
@@ -0,0 +1,4 @@
+import { liveQuerySelectorAll } from "../common";
+
+// Expand task descriptions automatically
+liveQuerySelectorAll(document, ".description-content-fade-button", el => el.click());
