4a7bd183ec7c162596a4faaf80c0876dc95929f6
Francesc Rosas <francescrosasbosque@gmail.com>
2017-12-29 12:04:21 +0100

    Inline UI into entry

--- js/hacker-news/entry.js
+++ js/hacker-news/entry.js
@@ -1,10 +1,19 @@
 // Shows only the most replied comments in https://news.ycombinator.com post and
 // comment pages.
 
-import UI from "./UI";
+import React from "react";
+import ReactDOM from "react-dom";
 import { getComments } from "./comments";
+import MainComponent from "./ui/MainComponent";
+
+const h = React.createElement;
 
 const comments = getComments();
 
 // Don't show the UI if the page has no comments (e.g. the homepage)
-if (comments.length) new UI({ comments, maxCommentsRatio: 0.05 });
+if (comments.length) {
+  const el = document.createElement('div');
+  el.className = 'chrome-customizations-extension-controls';
+  document.body.appendChild(el);
+  ReactDOM.render(h(MainComponent, { comments, initialMaxCommentsRatio: 0.05 }), el);  
+}
