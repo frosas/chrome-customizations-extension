4f2719a3740d8d9de482a70b11787e36e5bbf342
Francesc Rosas <francescrosasbosque@gmail.com>
2017-12-29 11:13:37 +0100

    Extract UI#_render()

--- js/hacker-news/UI.js
+++ js/hacker-news/UI.js
@@ -7,16 +7,10 @@ const h = React.createElement;
 
 export default class UI {
   constructor({ comments, maxCommentsRatio }) {
-    ReactDOM.render(
-      h("div", {},
-        h(MaxCommentsRatioControl, {
-          initialMax: maxCommentsRatio,
-          total: comments.length,
-          onChange: maxRatio => showOnlyMostReplied({ comments, maxRatio })
-        })
-      ),
-      this._createElement()
-    );  
+    this._comments = comments;
+    this._maxCommentsRatio = maxCommentsRatio;
+    this._element = this._createElement();
+    this._render();
   }
 
   _createElement() {
@@ -24,5 +18,20 @@ export default class UI {
     el.className = 'chrome-customizations-extension-controls';
     document.body.appendChild(el);
     return el;
+  }
+
+  _render() {
+    ReactDOM.render(
+      h("div", {},
+        h(MaxCommentsRatioControl, {
+          initialMax: this._maxCommentsRatio,
+          total: this._comments.length,
+          onChange: maxRatio => {
+            showOnlyMostReplied({ comments: this._comments, maxRatio });
+          }
+        })
+      ),
+      this._element
+    );  
   }
 }
