b39aadf0f7ca11d6d13bc8278c4a5d67ecf68b86
Francesc Rosas <francescrosasbosque@gmail.com>
2017-12-29 13:03:12 +0100

    Refact

--- js/hacker-news/ui/LegendComponent.js
+++ js/hacker-news/ui/LegendComponent.js
@@ -4,7 +4,7 @@ import { humanRatio } from "../comments";
 export default props => {
   return h("p", {},
     `Showing the ${humanRatio(props.maxCommentsRatio)}, and at least 
-    ${props.minComments}, of the most replied comments  (${props.shownComments} 
+    ${props.minComments}, of the most replied comments (${props.shownComments} 
     of ${props.totalComments}).`
   );
 };
