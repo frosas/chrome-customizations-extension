54f116153bf71e085a85dee9d196108f9f7ac409
Francesc Rosas <francescrosasbosque@gmail.com>
2019-02-09 01:17:32 +0000

    Remove unnecessary code

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,11 @@
 const { promisify } = require("util");
 const glob = require("glob");
 const { basename, dirname } = require("path");
-const { DefinePlugin } = require("webpack");
 
 module.exports = async () => ({
   entry: (await promisify(glob)("./js/**/entry.js")).reduce((entries, file) => {
     const name = basename(dirname(file)); // Use the parent dir name
     return { ...entries, [name]: file };
   }, {}),
-  output: { filename: "scripts/[name].js" },
-  plugins: [
-    // For React
-    new DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") })
-  ]
+  output: { filename: "scripts/[name].js" }
 });
