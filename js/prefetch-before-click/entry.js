9971478ee153e94a6c8b328561c32bafcfd80e38
Francesc Rosas <francescrosasbosque@gmail.com>
2019-02-10 12:06:35 +0000

    Preconnect to referred hrefs

---
+++ js/prefetch-before-click/entry.js
@@ -0,0 +1,31 @@
+// Inspired in https://instant.page/
+// https://w3c.github.io/resource-hints/ (API)
+
+const log = message => console.log(`[preemptive-prefetch] ${message}`);
+
+const appendLinkEl = attributes =>
+  document.head.appendChild(
+    Object.assign(document.createElement("link"), attributes)
+  );
+
+const preconnect = url => {
+  log(`Preconnecting to ${url}`);
+  appendLinkEl({ rel: "preconnect", href: url });
+};
+
+/**
+ * @returns (string|undefined) The origin of an HTTP(S) URL, or `undefined` otherwise
+ */
+const getHttpOrigin = urlString => {
+  const url = new URL(urlString);
+  if (url.protocol.match(/^https?:/)) return `${url.protocol}//${url.host}`;
+};
+
+const origins = new Set(
+  [...document.querySelectorAll("[href]")]
+    .map(el => getHttpOrigin(el.href))
+    .filter(origin => origin)
+);
+// There's no point on trying to preconnect to the own origin
+origins.delete(location.origin);
+[...origins].forEach(preconnect);
