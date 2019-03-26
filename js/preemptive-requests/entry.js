07fe7bf87f5e70d2a7cae23223458acd36e1b87d
Francesc Rosas <francescrosasbosque@gmail.com>
2019-03-26 10:33:33 +0000

    Revert "Use quicklink"
    This caused problems with Jira
    
    This reverts commit c3c2b777368713afc15f3d3a311d3f68e0e897e3.

--- js/preemptive-requests/entry.js
+++ js/preemptive-requests/entry.js
@@ -1,3 +1,31 @@
-import quicklink from "quicklink";
+// Inspired in https://instant.page/
+// https://w3c.github.io/resource-hints/ (API)
 
-quicklink();
+const log = message => console.log(`[preemptive-requests] ${message}`);
+
+const appendLinkEl = attributes =>
+  document.head.appendChild(
+    Object.assign(document.createElement("link"), attributes)
+  );
+
+const preconnect = url => {
+  log(`Preconnecting to ${url}`);
+  appendLinkEl({ rel: "preconnect", href: url });
+};
+
+/**
+ * @returns (string|undefined) The origin of an HTTP(S) URL, or `undefined` otherwise
+ */
+const getHttpOrigin = urlString => {
+  const url = new URL(urlString);
+  if (url.protocol.match(/^https?:/)) return `${url.protocol}//${url.host}`;
+};
+
+const origins = new Set(
+  [...document.querySelectorAll("a[href]")]
+    .map(el => getHttpOrigin(el.href))
+    .filter(origin => origin)
+);
+// There's no point on trying to preconnect to the own origin
+origins.delete(location.origin);
+[...origins].forEach(preconnect);
