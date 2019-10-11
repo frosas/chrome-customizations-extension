e555429e1c636ac1033585650dff4ebeeffe0ca5
Francesc Rosas <francescrosasbosque@gmail.com>
2019-10-11 13:26:24 +0100

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// See https://eslint.org/docs/user-guide/configuring
+module.exports = {
+  extends: ["@frosas/eslint-config"],
+  parserOptions: {
+    // For an ECMAScript version appropriate to a given Node version, check
+    // https://node.green
+    ecmaVersion: 2020
+  },
+  env: {
+    es6: true,
+    node: true
+  }
+};
---
+++ js/.eslintrc.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+
+module.exports = {
+  parserOptions: {
+    sourceType: "module"
+  },
+  env: {
+    node: false,
+    browser: true
+  }
+};
