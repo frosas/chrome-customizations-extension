b2e166fa4b109ed0a6c3eed56a37c5b9089d8594
Francesc Rosas <francesc@francescrosas.com>
2021-03-02 21:38:16 +0100

    Fix due date modal being broken in the new design

--- js/trello/entry.js
+++
@@ -1,6 +0,0 @@
-import { liveQuerySelectorAll } from "../common";
-
-// Expand task descriptions automatically
-liveQuerySelectorAll(document, ".description-content-fade-button", (el) => {
-  el.click();
-});
