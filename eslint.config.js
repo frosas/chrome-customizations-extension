09ab975482c167ceef5b40c11ba7a88d61cd099d
Francesc Rosas <francesc@francescrosas.com>
2024-04-09 17:46:00 +0100

    Update @frosas/eslint-config

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+// See https://eslint.org/docs/latest/use/configure/
+
+import globals from "globals"
+import frosasConfig from "@frosas/eslint-config"
+
+/** @type import('eslint').Linter.FlatConfig[] */
+export default [
+  ...frosasConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        // For an ECMAScript version appropriate to a given Node version, check
+        // https://node.green
+        ecmaVersion: 2020,
+      },
+      globals: { ...globals.es2020 },
+    },
+  },
+  {
+    files: ["js/**/*"],
+    languageOptions: {
+      parserOptions: { sourceType: "module" },
+      globals: { ...globals.browser },
+    },
+  },
+  { ignores: ["dist/"] },
+]
