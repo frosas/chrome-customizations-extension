e0630cda77daa25faab0da384c661985ef3ec846
Francesc Rosas <francesc@francescrosas.com>
2024-04-18 18:27:39 +0100

    Update dependency glob to v10

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
-import { promisify } from "util"
-import glob from "glob"
+import { glob } from "glob"
 import { basename, dirname } from "path"
 
 export default async () => ({
-  entry: (await promisify(glob)("./js/**/entry.js")).reduce((entries, file) => {
+  entry: (await glob("js/**/entry.js")).reduce((entries, file) => {
     const name = basename(dirname(file)) // Use the parent dir name
-    return { ...entries, [name]: file }
+    return { ...entries, [name]: `./${file}` }
   }, {}),
   output: { filename: "scripts/[name].js" },
 })
